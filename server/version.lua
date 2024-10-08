681d876404a4eab4433ab05768c3f23013c93189
whoissw <rafaelsesanaperdiz@gmail.com>
2024-10-08 14:50:28 -0300

    Refactor version.lua to update remote version URL

--- server/version.lua
+++ server/version.lua
@@ -1,46 +1,33 @@
---- Reads the local version from the version.txt file.
----@return string|nil The local version or nil if the file cannot be read.
-function readLocalVersion()
-    local file = io.open("version.txt","r")
-    if not file then
-        return nil
-    end
-    local version = file:read("*a")
-    file:close()
-    return version:match("^%s*(.-)%s*$")
+--- Logs a message with a specific type and color.
+---@param _type string The type of the log message ("success" or "error").
+---@param log string The log message to print.
+function VersionLog(_type,log)
+    local color = _type == "success" and "^2" or "^1"
+    print(("^8[wall]%s %s^7"):format(color,log))
 end
 
---- URL of the remote version file on GitHub.
----@type string
-local remoteVersionUrl = "https://raw.githubusercontent.com/whoissw/wall-system/main/version.txt"
-
---- The local version read from the version.txt file.
----@type string|nil
-local localVersion = readLocalVersion()
-if not localVersion then
-    print("Erro ao ler a versão local.")
-    return
-end
-
---- Compares the local and remote versions and prints the result.
----@param localVersion string The local version.
----@param remoteVersion string The remote version.
+--- Checks the current version of the menu against the latest version available on GitHub.
 ---@return void
-function compareVersions(localVersion,remoteVersion)
-    if localVersion == remoteVersion then
-        print("O sistema está atualizado. Versão: "..localVersion)
-    else
-        print("O sistema está desatualizado. Versão local: "..localVersion..", Versão remota: "..remoteVersion)
-    end
+function CheckMenuVersion()
+    local url = "https://raw.githubusercontent.com/whoissw/wall-system/main/version.txt"
+    PerformHttpRequest(url,function(err,text,headers)
+        local currentVersion = GetResourceMetadata(GetCurrentResourceName(),"version")
+
+        if not text then
+            VersionLog("error","Currently unable to run a version check.")
+            return
+        end
+
+        VersionLog("success",("Current Version: %s"):format(currentVersion))
+        VersionLog("success",("Latest Version: %s"):format(text))
+
+        if text:gsub("%s+","") == currentVersion:gsub("%s+","") then
+            VersionLog("success","You are running the latest version.")
+        else
+            VersionLog("error",("You are currently running an outdated version: %s"):format(text))
+        end
+    end)
 end
 
---- Performs an HTTP request to get the remote version and compares it with the local version.
----@return void
-PerformHttpRequest(remoteVersionUrl,function(statusCode,response,headers)
-    if statusCode == 200 then
-        local remoteVersion = response:match("^%s*(.-)%s*$")
-        compareVersions(localVersion,remoteVersion)
-    else
-        print("Erro ao obter a versão remota. Status code: "..statusCode)
-    end
-end,"GET","",{ ["Content-Type"] = "text/plain" })
+-- Initiates the version check.
+CheckMenuVersion()
